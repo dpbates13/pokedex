ada18d86b13d4365e41c96a84f7c65c19976cf6e
Dustin Bates <dpbates13@gmail.com>
2020-05-23 19:18:41 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+
+console.log(process.env.API_TOKEN);
+
+const app = express();
+
+app.use(morgan("dev"));
+
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get("/types", handleGetTypes);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
